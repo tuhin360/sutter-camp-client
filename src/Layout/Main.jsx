2035762b72836af8e490c272c34b290cc7a135da
tuhin <tuhin153695@gmail.com>
2023-06-14 08:09:53 +0600

    footer added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+
+ 
+
+const Main = () => {
+    return (
+        <div>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
